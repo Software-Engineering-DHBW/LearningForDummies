779eb8189695fb9798d332caf4aa5f9323a3f592
yShizu <82947072+yShizu@users.noreply.github.com>
2022-03-26 14:15:04 +0100

    Add test setup for SaveData

---
+++ LearningForDummies/Assets/Editor/SaveDataTests.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class SaveDataTests
+{
+    [Test]
+    public void QuestionCatalogue()
+    {
+        // Arrange: Daten und Device Under Test erstellen
+
+
+
+        // Act: Zu testende Methode vom DUT aufrufen
+
+
+
+        // Assert: Resultate mit Sollwerten vergleichen
+
+    }
+
+    public void Question()
+    {
+        // Arrange: Daten und Device Under Test erstellen
+
+
+
+        // Act: Zu testende Methode vom DUT aufrufen
+
+
+
+        // Assert: Resultate mit Sollwerten vergleichen
+
+    }
+
+    public void PlayerProfile()
+    {
+        // Arrange: Daten und Device Under Test erstellen
+
+
+
+        // Act: Zu testende Methode vom DUT aufrufen
+
+
+
+        // Assert: Resultate mit Sollwerten vergleichen
+
+    }
+
+    public void Statistic_Test()
+    {
+        // Arrange: Daten und Device Under Test erstellen
+
+
+
+        // Act: Zu testende Methode vom DUT aufrufen
+
+
+
+        // Assert: Resultate mit Sollwerten vergleichen
+
+    }
+
+}
+
+
+
+
+
+
+
+
