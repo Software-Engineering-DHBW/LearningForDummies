a9a324d97eec93a0219adfeb1f0013cd862bbdf4
EvilBunny333 <82946261+EvilBunny333@users.noreply.github.com>
2022-03-28 13:31:53 +0200

    add null tests for saveSystem

--- LearningForDummies/Assets/Editor/Tests/Test.cs
+++ LearningForDummies/Assets/Editor/Tests/Test.cs
@@ -6,13 +6,36 @@ using UnityEngine.TestTools;
 
 public class Test
 {
-    // A Test behaves as an ordinary method
+    // We only need to test string because the method cant get anything else
     [Test]
     [TestCase("Test")]
-    public void loadReturnsNull(string catalogueName)
+    [TestCase("5")]
+    [TestCase("I am me")]
+    [TestCase("%$§&")]
+    [TestCase("")]
+    public void loadRawReturnsNull(string catalogueName)
     {
         SaveSystem saveSystem = new SaveSystem();
         var answer = saveSystem.loadTextRawFromJson(catalogueName);
+        Assert.AreEqual(answer, null);
+    }
+    [Test]
+    [TestCase("Test")]
+    [TestCase("5")]
+    [TestCase("I am me")]
+    [TestCase("%$§&")]
+    [TestCase("")]
+    public void loadPlayerProfileReturnsNull(string profileName)
+    {
+        SaveSystem saveSystem = new SaveSystem();
+        var answer = saveSystem.loadPlayerProfileFromJson(profileName);
+        Assert.AreEqual(answer, null);
+    }
+    [Test]
+    public void loadCatalougesReturnsNull()
+    {
+        SaveSystem saveSystem = new SaveSystem();
+        var answer = saveSystem.loadQuestionCataloguesFromJson();
         Assert.AreEqual(answer, null);
     }
     
